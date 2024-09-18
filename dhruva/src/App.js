cfb2ac4907a1124b5f1efc642434e1ae9c515834
alkeshthedeveloper <alkeshsakre110791@gmail.com>
2024-09-18 22:53:37 +0530

    init commit
    Sass added

---
+++ dhruva/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Homepage from "./component/Homepage";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Homepage />
+    </>
+  );
+};
+
+export default App;
---
+++ dhruva/src/component/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.scss";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <h1>React Demo</h1>
+    </div>
+  );
+};
+
+export default Homepage;
