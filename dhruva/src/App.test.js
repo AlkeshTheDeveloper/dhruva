b204803e60eee74b2ceedf60e4af69a2f85d908d
alkeshthedeveloper <alkeshsakre110791@gmail.com>
2024-09-19 00:40:04 +0530

    New changes

--- dhruva/src/App.test.js
+++ dhruva/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("React Demo", () => {
+  beforeAll(() => {
+    jest.spyOn(console, "warn").mockImplementation(() => {});
+    jest.spyOn(console, "error").mockImplementation(() => {});
+  });
+
+  afterAll(() => {
+    console.warn.mockRestore();
+    console.error.mockRestore();
+  });
+
+  it("displays title in page", () => {
+    render(<App />); // Render the component
+    const title = screen.getByText("React Demo"); // Find the title element
+    expect(title).toMatchSnapshot(); // Snapshot the specific element
+  });
 });
--- dhruva/src/component/Homepage.jsx
+++ dhruva/src/component/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./HomePage.scss";
+import { Button, Col, Form, Nav, NavDropdown, Row } from "react-bootstrap";
 
 const Homepage = () => {
   return (
     <div className="container">
       <h1>React Demo</h1>
+      <div className="w-100 border border-primary rounded m-1 bg-light main-body text-center d-flex justify-content-between "></div>
     </div>
   );
 };
